46d10d40af238f6441c2f0059f5d662cd9a6fa90
Jacob Larson <jacobelarson@gmail.com>
2023-10-05 15:02:06 -0700

    Registered Album, Artist, Playlist, and Item models in admin

--- src/playlistgenerator/admin.py
+++ src/playlistgenerator/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Album, Artist, Playlist, Item
+
+admin.site.register(Album, admin.ModelAdmin)
+admin.site.register(Artist, admin.ModelAdmin)
+admin.site.register(Playlist, admin.ModelAdmin)
+admin.site.register(Item, admin.ModelAdmin)
