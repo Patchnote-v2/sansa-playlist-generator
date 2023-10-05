c00d8fef0666e5efe11a13e0fae0ad34c1a7b0aa
Jacob Larson <jacobelarson@gmail.com>
2023-10-05 15:55:24 -0700

    Moved to unified Album/Artist model to simplify ForeignKeys

--- src/playlistgenerator/admin.py
+++ src/playlistgenerator/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Album, Artist, Playlist, Item
+from .models import Item, Playlist, PlaylistRow
 
-admin.site.register(Album, admin.ModelAdmin)
-admin.site.register(Artist, admin.ModelAdmin)
-admin.site.register(Playlist, admin.ModelAdmin)
 admin.site.register(Item, admin.ModelAdmin)
+admin.site.register(Playlist, admin.ModelAdmin)
+admin.site.register(PlaylistRow, admin.ModelAdmin)
--- src/playlistgenerator/models.py
+++ src/playlistgenerator/models.py
@@ -3,30 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Artist(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    artist_name = models.CharField(max_length=256, blank=False, null=False)
-
-    def __str__(self):
-        return self.artist_name
-
-
-class Album(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    album_name = models.CharField(max_length=256, blank=False, null=False)
-    artists = models.ManyToManyField(Artist,
-                                     related_name="albums",
-                                     related_query_name="album")
-
-    def __str__(self):
-        return self.album_name
-
-
-class Playlist(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
 class Item(models.Model):
     ARTIST = "ARTIST"
     ALBUM = "ALBUM"
@@ -34,17 +10,50 @@ class Item(models.Model):
         (ARTIST, 'Artist'),
         (ALBUM, 'Album'),
     )
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=256, blank=False, null=True)
+
     type = models.CharField(max_length=16,
                             choices=TYPE_CHOICES,
                             blank=False,
                             null=True,
                             default=ALBUM)
 
-    amount = models.IntegerField(default=0, blank=False, null=True)
+    # If type == ARTIST, this must be empty
+    artists = models.ManyToManyField("Item",
+                                     related_name="artists_set",
+                                     related_query_name="artist")
+
+    # If type == ALBUM, this must be empty
+    albums = models.ManyToManyField("Item",
+                                    related_name="albums_set",
+                                    related_query_name="album")
+
+    def __str__(self):
+        return self.name
+
+
+class Playlist(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class PlaylistRow(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     playlist = models.ForeignKey(Playlist,
                                  on_delete=models.CASCADE,
                                  blank=False,
                                  null=True,
-                                 related_name="items",
-                                 related_query_name="item")
+                                 related_name="rows",
+                                 related_query_name="row")
+
+    item = models.ForeignKey(Item,
+                             on_delete=models.CASCADE,
+                             blank=False,
+                             null=True,
+                             related_name="items",
+                             related_query_name="item")
+
+    amount = models.IntegerField(default=0, blank=False, null=True)
