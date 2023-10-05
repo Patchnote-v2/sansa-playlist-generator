59c44d27c948f350c8819c9d70ff8e59132bd915
Jacob Larson <jacobelarson@gmail.com>
2023-10-05 15:00:40 -0700

    Added Playlist model and Item ForeignKey to Playlist

--- src/playlistgenerator/models.py
+++ src/playlistgenerator/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Artist(models.Model):
@@ -21,6 +22,11 @@ class Album(models.Model):
         return self.album_name
 
 
+class Playlist(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 class Item(models.Model):
     ARTIST = "ARTIST"
     ALBUM = "ALBUM"
@@ -36,3 +42,9 @@ class Item(models.Model):
                             default=ALBUM)
 
     amount = models.IntegerField(default=0, blank=False, null=True)
+    playlist = models.ForeignKey(Playlist,
+                                 on_delete=models.CASCADE,
+                                 blank=False,
+                                 null=True,
+                                 related_name="items",
+                                 related_query_name="item")
